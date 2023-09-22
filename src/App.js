6a7cab9eb2a3e0c440f76d17bd33f79684586ac7
nidhi1812 <nidhi18gupta12@gmail.com>
2023-09-22 15:05:45 -0400

    react-install

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
-const head = React.createElement( 'h1' , {id:'heading'} , "Helo nidhi from react");
-const root  =  ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+const head = React.createElement('h1', { id: 'heading' }, "Helo nidhi from react");
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(head)
